3c254dbc622b2e52140f7066f0daa8f0602b0260
ALiaksandr Pasynkau <alxe.master@gmail.com>
2015-07-25 23:54:51 +0300

    remove async dependency. change all async code on promises

--- index.js
+++ index.js
@@ -1,27 +1,23 @@
 "use strict";
 
 var _ = require('lodash');
-var async = require('async');
 var delay = require('./lib/delay');
 var config = require('./config');
 var logCsv = require('./modules/log-csv');
-
-//var detector = require('./modules/detector');
 var device = require('./modules/device');
 
 var logFile = logCsv(config.log.cvsFile);
 
-async.series([
-	function (done) {
-		device.reset().then(function () {
-			console.log('\nreset to zero\n');
-			done();
-		}, done);
-	},
-	function (done) {
-		setTimeout(done, config.device.initDelay);
-	},
-	function (done) {
+device
+	.reset()
+	.then(function () {
+		console.log('\nreset to zero\n');
+
+		return new Promise(function (done) {
+			setTimeout(done, config.device.initDelay);
+		});
+	})
+	.then(function () {
 		console.log('>> start loop');
 
 		var startedAt = Date.now();
@@ -31,77 +27,59 @@ async.series([
 		var func = require('./run/function');
 		var data = {};
 
-		delay
-			.reqTimer(config.device.checkInterval, function () {
-				return Promise.all([
-					device.readCurrentValues({ voltage: data.voltage, current: data.current }),
-					device.readSettingValues({ voltage: data.setting_voltage, current: data.setting_current })
-				]).then(function (results) {
-					number++;
-					var currentValues = results[0];
-					var settingValues = results[1];
-					var _settingValues = _.clone(results[1]);
-					var currentDate = Date.now();
+		return delay.reqTimer(config.device.checkInterval, function () {
+			return Promise.all([
+				device.readCurrentValues({ voltage: data.voltage, current: data.current }),
+				device.readSettingValues({ voltage: data.setting_voltage, current: data.setting_current })
+			]).then(function (results) {
+				number++;
+				var currentValues = results[0];
+				var settingValues = results[1];
+				var _settingValues = _.clone(results[1]);
+				var currentDate = Date.now();
 
-					var date = {
-						start: startedAt,
-						time: Date.now() - startedAt,
-						interval: currentDate - prevDate,
-						prev: prevDate
-					};
+				var date = {
+					start: startedAt,
+					time: Date.now() - startedAt,
+					interval: currentDate - prevDate,
+					prev: prevDate
+				};
 
-					data = {
-						n: number,
-						time: date.time / 1000,
-						voltage: currentValues.voltage,
-						current: currentValues.current,
-						setting_voltage: settingValues.voltage,
-						setting_current: settingValues.current
-					};
+				data = {
+					n: number,
+					time: date.time / 1000,
+					voltage: currentValues.voltage,
+					current: currentValues.current,
+					setting_voltage: settingValues.voltage,
+					setting_current: settingValues.current
+				};
 
-					var newSettingValues = func(settingValues, currentValues, date, number);
+				var newSettingValues = func(settingValues, currentValues, date, number);
 
-					data.func_voltage = newSettingValues.voltage;
-					data.func_current = newSettingValues.current;
+				data.func_voltage = newSettingValues.voltage;
+				data.func_current = newSettingValues.current;
 
-					prevDate = currentDate;
+				prevDate = currentDate;
 
-					console.log(date.interval, data);
+				console.log(date.interval, data);
 
-					if (_settingValues.voltage != newSettingValues.voltage || _settingValues.current != newSettingValues.current) {
-						return device.writeSettingValues(newSettingValues).then(function () {
-							return logFile.commit(data);
-						})
-					}
+				if (_settingValues.voltage != newSettingValues.voltage || _settingValues.current != newSettingValues.current) {
+					return device.writeSettingValues(newSettingValues).then(function () {
+						return logFile.commit(data);
+					})
+				}
 
-					return logFile.commit(data);
-				});
-			})
-			.on('error', done)
-			.on('end', done);
-	}/*,
-	function (done) {
-		detector(function (err, dataEmitter) {
-			if (err) {
-				done(err);
-				return;
-			}
-
-			console.log('init connect with sensors !');
-
-			dataEmitter.on('error', done);
-			dataEmitter.on('data', function (values) {
-				console.log(values);
+				return logFile.commit(data);
 			});
 		});
-	}*/
-], function (err) {
-	device.reset().then(function () {
-		console.log('\nreset to zero\n');
-		console.error('END:> !!!!', err);
-		process.exit();
+	})
+	.then(device.reset, function (err) {
+		return device.reset().then(function () {
+			console.log('\nreset to zero\n');
+			console.error('END:> !!!!', err);
+			process.exit();
+		});
 	});
-});
 
 process.on('SIGINT', function () {
 	device.reset().then(function () {
